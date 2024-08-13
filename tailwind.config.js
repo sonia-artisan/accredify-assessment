14eb9f4681f34d88b6e2c80aa3920a0305189e64
Sonia William <sonia@artisan.com.my>
2024-08-13 18:08:44 +0800

    define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#493DF5",
+        subtitle: "#5B6270",
+        sidebar: {
+          background: "#151F32",
+          active: "#353D4E",
+        },
+        icon: {
+          active: "#fff",
+          inactive: "#D0D2D6",
+        } 
+      }
+    },
   },
   plugins: [],
 }
