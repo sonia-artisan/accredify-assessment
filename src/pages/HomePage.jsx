6abd445ec79b472f6dc24608908b9cb0f34dabe8
Sonia William <sonia@artisan.com.my>
2024-08-14 18:33:23 +0800

    add homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Title from '../components/Title';
+import Subtitle from '../components/Subtitle';
+
+const HomePage = () => {
+  return (
+    <div><Title titleText='Hi, Gerald Goh' />
+    <Subtitle subtitleText='Manage your documents issued by SMU Academy or track your career goal.' /></div>
+  )
+}
+
+export default HomePage;
