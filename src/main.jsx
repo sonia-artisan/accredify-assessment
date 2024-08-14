ab3ffe258ef139301b46df5fd7a63fed6a22f432
Sonia William <sonia@artisan.com.my>
2024-08-14 17:56:03 +0800

    install scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css';
+import './index.scss';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.jsx';
 
