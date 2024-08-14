9dc4ec194b6ac09c37729fc88af4dfdd73dada83
Sonia William <sonia@artisan.com.my>
2024-08-14 18:33:12 +0800

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import MainLayout from './components/layouts/MainLayout';
-import Title from './components/Title';
-import Subtitle from './components/Subtitle';
+import HomePage from "./pages/HomePage";
 
 function App() {
 	return (
-		<MainLayout>
-			<Title titleText='Hi, Gerald Goh' />
-			<Subtitle subtitleText='Manage your documents issued by SMU Academy or track your career goal.' />
-		</MainLayout>
+		<Router>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Router>
 	);
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 import './index.scss';
 import { Provider } from 'react-redux';
@@ -9,9 +8,7 @@ import { store } from './redux/store.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <Router>
         <App />
-      </Router>
     </Provider>
   </StrictMode>,
 )
